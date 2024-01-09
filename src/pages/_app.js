d13db07ba0c54c4c0649854d42edb7e5b9e0ae18
saohwan <saohwan@gmail.com>
2024-01-09 16:41:12 +0900

    완료

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,8 @@ import {Montserrat} from "next/font/google";
 import Head from "next/head";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import {AnimatePresence} from "framer-motion";
+import {useRouter} from "next/router";
 // import NavBar from "next/head";
 
 const montserrat = Montserrat({
@@ -11,6 +13,7 @@ const montserrat = Montserrat({
 });
 
 export default function App({Component, pageProps}) {
+    const router = useRouter();
     return (
         <>
             <Head>
@@ -18,10 +21,12 @@ export default function App({Component, pageProps}) {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main
-                className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+                className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
             >
                 <NavBar/>
-                <Component {...pageProps} />
+                <AnimatePresence mode="wait">
+                    <Component key={router.asPath} {...pageProps} />
+                </AnimatePresence>
                 <Footer/>
             </main>
         </>
--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,9 @@ import Image from "next/image";
 import profilePic from "../../public/images/profile/developer-pic-2.jpg"
 import {useInView, useMotionValue, useSpring} from "framer-motion";
 import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+import TransitionEffect from "@/components/TransitionEffect";
 
 
 const AnimatedNumbers = ({value}) => {
@@ -34,78 +37,93 @@ const AnimatedNumbers = ({value}) => {
 
 
 const about = () => {
-    return (
-        <>
-            <Head>
-                <title>saohwan | About Page</title>
-                <meta name="description" content="any description"/>
-            </Head>
-            <main className="flex w-full flex-col items-center justify-center">
-                <Layout className='pt-16'>
-                    <AnimatedText text="Passion Fuels Purpose!" className="mb-16"/>
-                    <div className="grid w-full grid-cols-8 gap-16">
-                        <div className="col-span-3 flex flex-col items-start justify-start">
-                            <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">Biography</h2>
-                            <p className="font-medium">
-                                Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating
-                                beautiful, functional,
-                                and user-centered digital experiences. With 4 years of experience in the field. I am
-                                always looking for
-                                new and innovative ways to bring my clients' visions to life.
-                            </p>
-                            <p className="my-4 font-medium">
-                                I believe that design is about more than just making things look pretty – it's about
-                                solving problems and
-                                creating intuitive, enjoyable experiences for users.
-                            </p>
-                            <p className="my-4 font-medium">
-                                Whether I'm working on a website, mobile app, or
-                                other digital product, I bring my commitment to design excellence and user-centered
-                                thinking
-                                to
-                                every project I work on. I look forward to the opportunity to bring my skills and
-                                passion to
-                                your next project.
-                            </p>
-                        </div>
-                        <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light
-                        p-8
+    return (<>
+        <Head>
+            <title>saohwan | About Page</title>
+            <meta name="description" content="any description"/>
+        </Head>
+        <TransitionEffect/>
+        <main className="flex w-full flex-col items-center justify-center dark:text-light">
+            <Layout className='pt-16'>
+                <AnimatedText text="Passion Fuels Purpose!" className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl
+                sm:mb-8
+                "/>
+                <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+                    <div
+                        className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2
+                        md:col-span-8">
+                        <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">Biography</h2>
+                        <p className="font-medium">
+                            Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating
+                            beautiful, functional,
+                            and user-centered digital experiences. With 4 years of experience in the field. I am
+                            always looking for
+                            new and innovative ways to bring my clients' visions to life.
+                        </p>
+                        <p className="my-4 font-medium">
+                            I believe that design is about more than just making things look pretty – it's about
+                            solving problems and
+                            creating intuitive, enjoyable experiences for users.
+                        </p>
+                        <p className="my-4 font-medium">
+                            Whether I'm working on a website, mobile app, or
+                            other digital product, I bring my commitment to design excellence and user-centered
+                            thinking
+                            to
+                            every project I work on. I look forward to the opportunity to bring my skills and
+                            passion to
+                            your next project.
+                        </p>
+                    </div>
+                    <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light
+                        p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8
                         ">
-                            <div className="absolute top-0 -right-3 -z-10 w-[100%] h-[102%] rounded-[2rem] bg-dark"/>
-                            <Image src={profilePic} alt="Saohwan" className="w-full h-auto rounded-2xl"/>
-                        </div>
-                        <div className="col-span-2 flex flex-col items-end justify-between">
-                            <div className="flex flex-col items-end justify-center">
-                            <span className="inline-block text-7xl font-bold">
+                        <div className="absolute top-0 -right-3 -z-10 w-[100%] h-[102%] rounded-[2rem] bg-dark
+                            dark:bg-ligh"/>
+                        <Image src={profilePic} alt="Saohwan" className="w-full h-auto rounded-2xl"
+                               priority sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                        />
+
+                    </div>
+                    <div
+                        className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row
+                        xl:items-center md:order-3">
+                        <div className="flex flex-col items-end justify-center xl:items-center">
+                            <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                                 <AnimatedNumbers value={50}/>+
                             </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">
-                                    satisfied clients
-                                </h2>
-                            </div>
-                            <div className="flex flex-col items-end justify-center">
-                            <span className="inline-block text-7xl font-bold">
+                            <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75
+                             xl:text-center md:text-lg sm:text-base xs:text-sm">
+                                satisfied clients
+                            </h2>
+                        </div>
+                        <div className="flex flex-col items-end justify-center xl:items-center">
+                            <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                                 <AnimatedNumbers value={40}/>+
                             </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">
-                                    projects completed
-                                </h2>
-                            </div>
-                            <div className="flex flex-col items-end justify-center">
-                            <span className="inline-block text-7xl font-bold">
+                            <h2 className="text-xl font-medium capitalize text-dark/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
+                                projects completed
+                            </h2>
+                        </div>
+                        <div className="flex flex-col items-end justify-center xl:items-center">
+                            <span
+                                className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                                 <AnimatedNumbers value={4}/>+
                             </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">
-                                    years of experience clients
-                                </h2>
-                            </div>
+                            <h2 className="text-xl font-medium capitalize text-dark/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
+                                years of experience clients
+                            </h2>
                         </div>
                     </div>
-                    <Skills/>
-                </Layout>
-            </main>
-        </>
-    );
+                </div>
+                <Skills/>
+                <Experience/>
+                <Education/>
+            </Layout>
+        </main>
+    </>);
 };
 
 export default about;
